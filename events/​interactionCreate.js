84f22dad6cef114bbbfa486bd13ba31009946a4a
banidozzy-prog <banidozzy@gmail.com>
2026-06-13 22:33:01 -0300

    Update ​interactionCreate.js

--- events/​interactionCreate.js
+++ events/​interactionCreate.js
@@ -1,1 +1,5 @@
-
+module.exports = async (client, interaction) => {
+    if (!interaction.isButton()) return;
+    if (interaction.customId === 'wo') await interaction.reply("W.O. marcado!");
+    if (interaction.customId === 'fechar') await interaction.reply("Partida fechada!");
+};
