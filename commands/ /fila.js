1382f11dd68e791b64f2d472f41a0b742e5987d8
banidozzy-prog <banidozzy@gmail.com>
2026-06-14 17:43:30 -0300

    Update fila.js

--- commands/ /fila.js
+++ commands/ /fila.js
@@ -1,1 +1,14 @@
+const { ChannelType } = require('discord.js');
 
+module.exports = {
+    name: 'fila',
+    async execute(message) {
+        const categoria = message.guild.channels.cache.find(c => c.type === ChannelType.GuildCategory && c.children.cache.size < 8);
+        const channel = await message.guild.channels.create({
+            name: `fila-${message.author.username}`,
+            type: ChannelType.GuildText,
+            parent: categoria
+        });
+        message.reply(`Fila criada em ${channel}`);
+    }
+};
