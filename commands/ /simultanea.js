7c07a5d6bb5717bb6ce3a806960f6d9444a4064b
banidozzy-prog <banidozzy@gmail.com>
2026-06-14 17:44:00 -0300

    Update simultanea.js

--- commands/ /simultanea.js
+++ commands/ /simultanea.js
@@ -1,1 +1,15 @@
+const Partida = require('../models/Partida');
 
+module.exports = {
+    name: 'simultanea',
+    async execute(message) {
+        const total = await Partida.countDocuments({ status: 'ativa' });
+        message.reply({
+            embeds: [{
+                title: '<:simultanea:1514876848553725993> Filas Ativas',
+                description: `No momento existem **${total}** filas simultâneas ativas no servidor.`,
+                color: 0x00FF00
+            }]
+        });
+    }
+};
