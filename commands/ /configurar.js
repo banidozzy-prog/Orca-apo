a1bc7e2e5ca120a867b592cef3f79ba110f3f412
banidozzy-prog <banidozzy@gmail.com>
2026-06-13 22:37:25 -0300

    Update configurar.js

--- commands/ /configurar.js
+++ commands/ /configurar.js
@@ -1,1 +1,7 @@
-
+module.exports = {
+    name: 'configurar',
+    async execute(message) {
+        if (!message.member.permissions.has("ADMINISTRATOR")) return message.reply("Apenas ADMs.");
+        message.reply("⚙️ **Configuração:** O bot está operando em modo automático. Nenhuma configuração de ID necessária!");
+    }
+};
