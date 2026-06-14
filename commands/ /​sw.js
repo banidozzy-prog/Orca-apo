39244e2f73900676ec09b1c894dd68a65334da31
banidozzy-prog <banidozzy@gmail.com>
2026-06-13 22:33:30 -0300

    Update ​sw.js

--- commands/ /​sw.js
+++ commands/ /​sw.js
@@ -1,1 +1,12 @@
+const { ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
+module.exports = {
+    name: 'sw',
+    async execute(message) {
+        const row = new ActionRowBuilder().addComponents(
+            new ButtonBuilder().setCustomId('wo').setLabel('W.O.').setStyle(ButtonStyle.Danger),
+            new ButtonBuilder().setCustomId('fechar').setLabel('Fechar').setStyle(ButtonStyle.Success)
+        );
+        await message.reply({ content: 'Gerenciador da partida:', components: [row] });
+    }
+};
 
