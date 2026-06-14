0a4ba5b855ea13b9bab823cb83242600d0750b33
banidozzy-prog <banidozzy@gmail.com>
2026-06-13 22:45:12 -0300

    Update logger.js

--- utils/logger.js
+++ utils/logger.js
@@ -1,1 +1,8 @@
-
+module.exports = {
+    log: (message) => {
+        console.log(`[${new Date().toLocaleTimeString()}] ${message}`);
+    },
+    error: (message) => {
+        console.error(`[${new Date().toLocaleTimeString()}] ERRO: ${message}`);
+    }
+};
