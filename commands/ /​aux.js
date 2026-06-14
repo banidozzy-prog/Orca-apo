14f102e81b6d355f070eed8ccbd95f901a727c3b
banidozzy-prog <banidozzy@gmail.com>
2026-06-13 22:37:46 -0300

    Update ​aux.js

--- commands/ /​aux.js
+++ commands/ /​aux.js
@@ -1,1 +1,6 @@
-
+module.exports = {
+    name: 'aux',
+    async execute(message) {
+        message.reply("📜 **Comandos da ORCA APOSTAS:**\n`!sw` - Iniciar partida\n`!pix` - Sistema de pagamentos\n`!ss` - Suporte\n`!aux` - Ver esta ajuda");
+    }
+};
