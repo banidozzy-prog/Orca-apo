adbc636bf5f8ef039c5530d385ed06f734c93134
banidozzy-prog <banidozzy@gmail.com>
2026-06-13 22:38:12 -0300

    Update ​ss.js

--- commands/ /​ss.js
+++ commands/ /​ss.js
@@ -1,1 +1,6 @@
-
+module.exports = {
+    name: 'ss',
+    async execute(message) {
+        message.reply("🆘 **Chamado de Suporte:**\nUma mensagem foi enviada para a staff. Aguarde um momento no canal de voz.");
+    }
+};
